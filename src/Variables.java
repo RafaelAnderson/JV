54ec089026b5774eacbc289ab624624d5c6870dc
RafaelAnderson <rp_9966@hotmail.com>
2022-01-18 19:33:39 -0500

    Variables

---
+++ src/Variables.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class Variables {
+    public static void main(String[] args) {
+        String saludar = "Hola ";
+        System.out.println(saludar);
+        System.out.println("saludar.toUpperCase() = " + saludar.toUpperCase());
+        
+        int numero = 10;
+        System.out.println("numero = " + numero);
+
+        boolean valor = true;
+        if(valor) {
+            int numero2 = 15;
+            System.out.println("numero2 = " + numero2);
+        }
+        var numero3 = "20";
+        System.out.println("numero3 = " + numero3);
+    }
+}
