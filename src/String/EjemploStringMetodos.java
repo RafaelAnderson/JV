231ea015256ff96876f64f4e1fd22851c60e08ab
RafaelAnderson <rp_9966@hotmail.com>
2022-03-23 15:25:14 -0500

    Métodos string 2

--- src/String/EjemploStringMetodos.java
+++ src/String/EjemploStringMetodos.java
@@ -23,5 +23,18 @@ public class EjemploStringMetodos {
         System.out.println("nombre.substring(1, 4) = " + nombre.substring(1, 4));
         System.out.println("nombre.substring(4, 6) = " + nombre.substring(4, 6));
         System.out.println("nombre.substring(nombre.length()-1) = " + nombre.substring(nombre.length()-1));
+
+        /* Segunda parte */
+        String trabalenguas = "trabalenguas";
+        System.out.println("trabalenguas.replace(\"a\",\".\") = " + trabalenguas.replace("a","."));
+        System.out.println("trabalenguas.indexOf(\"f\") = " + trabalenguas.indexOf("a"));
+        System.out.println("trabalenguas.lastIndexOf(\"a\") = " + trabalenguas.lastIndexOf("a"));
+        System.out.println("trabalenguas.indexOf(\"z\") = " + trabalenguas.indexOf("z"));
+        System.out.println("trabalenguas.contains(\"a\") = " + trabalenguas.contains("a"));
+        System.out.println("trabalenguas.startsWith(\"lenguas\") = " + trabalenguas.startsWith("lenguas"));
+        System.out.println("trabalenguas.startsWith(\"traba\") = " + trabalenguas.startsWith("traba"));
+        System.out.println("trabalenguas.endsWith(\"lenguas\") = " + trabalenguas.endsWith("lenguas"));
+        System.out.println(" trabalenguas ");
+        System.out.println(" trabalenguas ".trim());
     }
 }
